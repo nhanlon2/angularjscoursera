5c851f8960a2b23c7170bd4a875e20670a0a3f8b
Neil Hanlon <NH185052@ncr.com>
2016-11-04 09:48:11 +0000

    WIP

--- assignment_5_solution/src/public/sign_up/signup.controller.js
+++ assignment_5_solution/src/public/sign_up/signup.controller.js
@@ -9,14 +9,21 @@ function SignUpController(MenuItemService) {
   var signup = this;
 
   signup.submit = function () {
-    var data = MenuItemService.getDataForMenuItem(signup.user.favorite);
-    if(data){
-      signup.completed = true;
-      signup.nosuchmenuitem=false;
-    }
-    else{
-      signup.nosuchmenuitem=true;
-    }
+    var promise = MenuItemService.getDataForMenuItem(signup.user.favorite);
+    var menuitemdata;
+    promise.then(function (response) {
+      menuitemdata = response.data;
+      if(menuitemdata){
+        signup.completed = true;
+        signup.nosuchmenuitem=false;
+      }
+      else{
+        signup.nosuchmenuitem=true;
+      }
+    })
+    .catch(function (error) {
+      console.log("Error contacting menu items server.");
+    });
   };
 }
 
